cda1f2c50f186a8f1f98edab6d7f3be866e87bcb
zhenxiang-shawn <zhenxiang.shawn@outlook.com>
2022-04-04 21:06:33 +0800

    Update Vector.py

--- Chapter01_Prologue/Vector.py
+++ Chapter01_Prologue/Vector.py
@@ -20,6 +20,13 @@ class Vector:
         x = self.x + other.x
         y = self.y + other.y
         return Vector(x, y)
-    
+
     def __mul__(self, scalar):
         return Vector(self.x * scalar, self.y * scalar)
+
+
+print((4 or 5))
+print((4 and 5))
+print(bool(4 or 5))
+
+# Python语言参考手册中的“Data Model”一章列出了83个特殊方法的名字，其中47个用于实现算术运算、位运算和比较操作
